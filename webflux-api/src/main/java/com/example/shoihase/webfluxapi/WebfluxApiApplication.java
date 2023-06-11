6474664f57fb0d9b705c894f5a67fd7b6efd0c2c
haseesah <hase1108shoiti@yahoo.co.jp>
2023-06-12 08:20:35 +0900

    add blank project

---
+++ webflux-api/src/main/java/com/example/shoihase/webfluxapi/WebfluxApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoihase.webfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApiApplication.class, args);
+	}
+
+}
